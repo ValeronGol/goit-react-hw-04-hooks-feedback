9d366f24fe1f77ce519bf90ae6ff1fd9f53793b0
valeraron <77055501+valeraron@users.noreply.github.com>
2021-09-20 19:11:00 +0300

    hooks well done

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,32 +1,16 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
-import shortid from 'shortid';
-import { Button } from './FeedbackOptions.styled';
+import { BtnFeedback } from 'components/FeedbackOptions/BtnFeedback';
 
-export default class FeedbackOptions extends Component {
-  feedbackAdd = event => {
-    this.props.onLeaveFeedback(event.target.getAttribute('name'));
-  };
-
-  render() {
-    const { options } = this.props;
-    return (
-      <>
-        {options.map(option => (
-          <Button
-            type="button"
-            name={option}
-            onClick={this.feedbackAdd}
-            key={shortid.generate()}
-          >
-            {option}
-          </Button>
-        ))}
-      </>
-    );
-  }
+export default function FeedbackOptions({ onLeaveFeedback }) {
+  return (
+    <>
+      <BtnFeedback text="Good" onClick={onLeaveFeedback} />
+      <BtnFeedback text="Neutral" onClick={onLeaveFeedback} />
+      <BtnFeedback text="Bad" onClick={onLeaveFeedback} />
+    </>
+  );
 }
+
 FeedbackOptions.propTypes = {
   onLeaveFeedback: PropTypes.func.isRequired,
-  options: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
